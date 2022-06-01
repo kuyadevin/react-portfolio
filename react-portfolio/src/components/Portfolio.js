21ec95c0f20ad96da4d706e7f8b7586a5ae4f6d9
KuyaDevin <devinnguyen@ucsb.edu>
2022-06-01 16:48:16 -0700

    Add cards for all six projects

--- react-portfolio/src/components/Portfolio.js
+++ react-portfolio/src/components/Portfolio.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Workday from '../assets/workday.png';
 import GitHub from '../assets/github.png';
 import MiseEnPlace from '../assets/MiseEnPlace.png';
+import TravelCenter from '../assets/travelCenter.png';
+import EMS from '../assets/ems.png';
+import ECommerce from '../assets/ecommerce.png';
+import Social from '../assets/socialmedia.png';
 
 function Portfolio() {
   return (
@@ -17,16 +21,46 @@ function Portfolio() {
               <img src={GitHub} alt='GitHub' style={{ width: '30px' }} />
             </a>
           </div>
-          <div><a href='https://mise-en-place-planner.herokuapp.com/'>
-            <img src={MiseEnPlace} alt='Mise En Place' style={{ width: '500px' }} />
-          </a>
+          <div>
+            <a href='https://mise-en-place-planner.herokuapp.com/'>
+              <img src={MiseEnPlace} alt='Mise En Place' style={{ width: '500px' }} />
+            </a>
             <a className="inline-block" href='https://github.com/jvbridge/mise-en-place'>
               <img src={GitHub} alt='GitHub' style={{ width: '30px' }} />
-            </a></div>
-          <div>3</div>
-          <div>4</div>
-          <div>5</div>
-          <div>6</div>
+            </a>
+          </div>
+          <div>
+            <a href='https://kuyadevin.github.io/the-travel-center/'>
+              <img src={TravelCenter} alt='Travel Center' style={{ width: '500px' }} />
+            </a>
+            <a className="inline-block" href='https://github.com/kuyadevin/the-travel-center'>
+              <img src={GitHub} alt='GitHub' style={{ width: '30px' }} />
+            </a>
+          </div>
+          <div>
+            <a href='https://github.com/kuyadevin/employee-management-system'>
+              <img src={EMS} alt='Employee Management System' style={{ width: '500px' }} />
+            </a>
+            <a className="inline-block" href='https://github.com/kuyadevin/employee-management-system'>
+              <img src={GitHub} alt='GitHub' style={{ width: '30px' }} />
+            </a>
+          </div>
+          <div>
+            <a href='https://github.com/kuyadevin/e-commerce-back-end'>
+              <img src={ECommerce} alt='E-Commerce' style={{ width: '500px' }} />
+            </a>
+            <a className="inline-block" href='https://github.com/kuyadevin/e-commerce-back-end'>
+              <img src={GitHub} alt='GitHub' style={{ width: '30px' }} />
+            </a>
+          </div>
+          <div>
+            <a href='https://github.com/kuyadevin/social-media-api'>
+              <img src={Social} alt='Social Media API' style={{ width: '500px' }} />
+            </a>
+            <a className="inline-block" href='https://github.com/kuyadevin/social-media-api'>
+              <img src={GitHub} alt='GitHub' style={{ width: '30px' }} />
+            </a>
+          </div>
         </div>
       </div>
     </div>
