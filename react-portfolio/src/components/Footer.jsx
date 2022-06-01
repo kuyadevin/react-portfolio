5275f406d42dbc38135e332a79806f94050a908a
KuyaDevin <devinnguyen@ucsb.edu>
2022-06-01 00:46:09 -0700

    Clean footer code and add space between logos

--- react-portfolio/src/components/Footer.jsx
+++ react-portfolio/src/components/Footer.jsx
@@ -4,15 +4,13 @@ import LinkedIn from '../assets/LI-In-Bug.png';
 
 function Footer() {
   return (
-    <footer className='flex text-center justify-between'>
-  <div className="absolute inset-x-0 bottom-0 text-center items-center text-gray-700 p-10">
+    <footer className='absolute inset-x-0 bottom-0 text-center space-x-4'>
     <a className="inline-block" href='https://github.com/kuyadevin'>
-      <img src={GitHub} alt='GitHub' style={{width: '80px'}}/>
+      <img src={GitHub} alt='GitHub' style={{width: '50px'}}/>
     </a>
     <a className="inline-block" href='https://www.linkedin.com/in/devin-nguyen-9a0676212/'>
-      <img src={LinkedIn} alt='LinkedIn' style={{width: '80px'}}/>
+      <img src={LinkedIn} alt='LinkedIn' style={{width: '50px'}}/>
     </a>
-  </div>
 </footer>
   )
 }
