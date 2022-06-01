e9d9af1477004957bb9894843bb9c7404d691d10
KuyaDevin <devinnguyen@ucsb.edu>
2022-05-31 21:46:07 -0700

    Create navigation jsx

---
+++ react-portfolio/src/components/Navigation.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#556b2f]'>
+
+    </div>
+  )
+}
+
+export default Navigation
